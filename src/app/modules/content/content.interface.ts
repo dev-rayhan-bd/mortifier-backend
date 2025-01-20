cd9ee03ea6e55fec27233489156f415eaebcc159
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-01-20 12:32:50 +0600

    done content interface, model and validation using zod

---
+++ src/app/modules/content/content.interface.ts
@@ -0,0 +1,11 @@
+import { Types } from "mongoose";
+
+export interface IContent {
+    title: string;
+    content: string;
+    specialism: string[];
+    status: "in-progress" | "blocked";
+    videoUrl?: string;
+    imageUrl?: string;
+    userId?: Types.ObjectId;
+}
---
+++ src/app/modules/content/content.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, model } from "mongoose";
+import { IContent } from "./content.interface";
+
+const contentSchema: Schema<IContent> = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    specialism: { type: [String], required: true },
+    status: { type: String, enum: ["in-progress", "blocked"], required: true },
+    videoUrl: { type: String, required: false },
+    imageUrl: { type: String, required: false },
+    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true }
+}, { timestamps: true });
+
+export const Content = model<IContent>("Content", contentSchema);
