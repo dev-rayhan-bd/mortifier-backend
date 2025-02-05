1912ffb8eb246f8ebc819479a3899fc95e243e7e
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-02-05 12:16:31 +0600

    did follow unfollow and get followers

--- src/app/modules/follower/follower.route.ts
+++ src/app/modules/follower/follower.route.ts
@@ -6,6 +6,6 @@ import { followAndUnfollowController } from "./follower.controller";
 const router = Router();
 
 router.post('/', auth(ENUM_USER_ROLE.TRAINEE, ENUM_USER_ROLE.TRAINER, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN), followAndUnfollowController.followAndUnfollow)
-router.get('/', auth(ENUM_USER_ROLE.TRAINEE, ENUM_USER_ROLE.TRAINER, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN), followAndUnfollowController.getMyfollower)
+router.get('/:id', auth(ENUM_USER_ROLE.TRAINEE, ENUM_USER_ROLE.TRAINER, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN), followAndUnfollowController.getMyfollower)
 
 export const followAndUnfollowRouter = router;
