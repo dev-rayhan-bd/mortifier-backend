91af77e8706452a6237d4ae6f684baf873f7de07
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-02-05 16:51:34 +0600

    get all trainer with filters and search and pagination

--- src/app/modules/trainer/trainer.controller.ts
+++ src/app/modules/trainer/trainer.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { trainerServices } from "./trainer.services";
+import { IPaginationOptions } from "../../global/globalType";
 
 const updateTriner = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -27,7 +28,32 @@ const updateTriner = async (req: Request, res: Response, next: NextFunction) => 
     }
 }
 
+const getAllTrainer = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { limit, page, sortBy, sortOrder, searchTerm, ...filters } = req.query;
+
+        const paginationOptions: IPaginationOptions = {
+            limit: Number(limit) || 10,
+            page: Number(page) || 1,
+            sortBy: sortBy?.toString() || 'createdAt',
+            sortOrder: sortOrder?.toString() === 'desc' ? 'desc' : 'asc',
+        };
+
+        const result = await trainerServices.getAllTrainer(paginationOptions, searchTerm, filters);
+
+        res.status(200).json({
+            success: true,
+            message: 'get all trainer successfully',
+            data: result,
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
 export const trainerController = {
     updateTriner,
+    getAllTrainer,
 }
 
--- src/app/modules/trainer/trainer.services.ts
+++ src/app/modules/trainer/trainer.services.ts
@@ -1,3 +1,5 @@
+import { IPaginationOptions } from "../../global/globalType";
+import { paginationHelpers } from "../../helpers/pagination";
 import { ITrainer } from "./trainer.interface";
 import { Trainer } from "./trainer.model";
 
@@ -20,6 +22,60 @@ const updateTrainer = async (file: any, id: string, data: Partial<ITrainer>) => 
 
 }
 
+const getAllTrainer = async (paginationOptions: IPaginationOptions, searchTerm: any, filtersData: any): Promise<any> => {
+    const { limit, page, skip, sortBy, sortOrder } = paginationHelpers.calculatePagination(paginationOptions);
+
+    const andConditions = [];
+
+
+    const contentSearchableFields = ["firstName", "lastName", "email", "consultationType"];
+
+    if (searchTerm) {
+        andConditions.push({
+            $or: contentSearchableFields.map((field) => ({
+                [field]: {
+                    $regex: searchTerm,
+                    $options: "i",
+                },
+            })),
+        });
+    }
+
+    if (Object.keys(filtersData).length) {
+        andConditions.push({
+            $and: Object.entries(filtersData).map(([field, value]) => ({
+                [field]: value,
+            })),
+        });
+    }
+
+
+    const sortConditions: { [key: string]: 1 | -1 } = {};
+    if (sortBy && sortOrder) {
+        sortConditions[sortBy] = sortOrder === 'asc' || sortOrder === 'ascending' ? 1 : -1;
+    }
+
+    const whereConditions = andConditions.length > 0 ? { $and: andConditions } : {};
+
+    const result = await Trainer.find(whereConditions)
+        .sort(sortConditions)
+        .skip(skip)
+        .limit(limit)
+    const total = await Trainer.countDocuments(whereConditions);
+
+    return {
+        meta: {
+            page,
+            limit,
+            total,
+        },
+        data: result,
+    };
+
+}
+
+
 export const trainerServices = {
     updateTrainer,
+    getAllTrainer,
 }
