377a9fe8a9cfa69d676880e4db4bdf75cb2132fc
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-02-22 10:42:31 +0600

    fixed token issue

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,15 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 initialChats(io)
 app.set("io",io)
-
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+};
 // parser
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(cookieParser());
 // Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
