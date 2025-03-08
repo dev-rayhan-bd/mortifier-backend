a665ba19d7b0c4d41173438419248d0288ef8582
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-03-08 14:53:22 +0600

    update chats

--- src/app/modules/chats/chats.interface.ts
+++ src/app/modules/chats/chats.interface.ts
@@ -4,6 +4,8 @@ export type IChat = {
     sender: Types.ObjectId; 
     receiver: Types.ObjectId;
     message: string;
+    deliveredTo: Types.ObjectId[]; // Tracks users who received the message
+    readBy: Types.ObjectId[]; // Tracks users who read the message
 };
 
 // export type IChatAdmin = {
