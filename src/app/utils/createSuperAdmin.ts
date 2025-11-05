fe2c0fe72511edc4b228f5c2a3d86deb167903b2
Shipon Hossen Raju <msshipon234@gmail.com>
2025-11-05 20:23:26 +0600

    admin creating issue solved

--- src/app/utils/createSuperAdmin.ts
+++ src/app/utils/createSuperAdmin.ts
@@ -1,27 +1,28 @@
-import config from "../config";
-import { Admin } from "../modules/admins/admin.model";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt'
+import config from '../config'
+import { Admin } from '../modules/admins/admin.model'
 
 const seedSuperAdmin = async () => {
-
-  const isSuperAdminExits = await Admin.findOne({ role: "super_admin" });
+  const isSuperAdminExits = await Admin.findOne({ role: 'super_admin' })
 
   if (!isSuperAdminExits) {
-    const hashedPassword = await bcrypt.hash("superAdmin777", Number(config.bcrypt_salt_rounds));
+    const hashedPassword = await bcrypt.hash(
+      'superAdmin777',
+      Number(config.bcrypt_salt_rounds),
+    )
 
     const superUser = {
-      firstName: "Super",
-      lastName: "Admin",
-      email: "superadmin777@gmail.com",
+      firstName: 'Super',
+      lastName: 'Admin',
+      email: 'superadmin777@gmail.com',
       password: hashedPassword,
-      role: "super_admin",
-      chatId: "admin77De5497Ed",
-      status: "in-progress",
-    };
+      role: 'super_admin',
+      chatId: 'admin77De5497Ed',
+      status: 'in-progress',
+    }
 
-    await Admin.create(superUser);
+    await Admin.create(superUser)
   }
+}
 
-};
-
-export default seedSuperAdmin;
+export default seedSuperAdmin
